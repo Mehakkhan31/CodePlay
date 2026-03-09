7c741cbf96cda845ca5cffa80e31955192b7fb8e
Mehakkhan31 <mehakkhan997740@gmail.com>
2026-03-09 14:45:43 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from "next/image";
+import { Button } from "@/components/ui/button"
 
 export default function Home() {
   return (
     <div>
-      <h1>mehak </h1>
+      <h1 className="font-game text-2xl">mehak </h1>
+      <Button>Subscribe</Button>
     </div>
   );
 }
